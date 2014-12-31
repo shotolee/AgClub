d5720adeaae0c8daabcb7f71ffdbacac6f420d35
shoto <shotolee@gmail.com>
2014-12-31 21:24:33 +0800

    add page mark

--- dailyaccount.php
+++ dailyaccount.php
@@ -1,64 +1,21 @@
-<!DOCTYPE html>
-<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
-<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
-<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
-<!--[if (gte IE 9)|!(IE)]><!--><html lang="zh-CN"> <!--<![endif]-->
-<head>
+<?php 
 
-	<!-- Basic Page Needs
-  ================================================== -->
-	<meta charset="utf-8">
-	<title>DailyAccount</title>
-	<meta name="description" content="">
-	<meta name="author" content="">
+$page_title = 'Haome&Leitu'; //页面标题
+include ('includes/header.html'); 
+?>
 
-	<!-- Mobile Specific Metas
-  ================================================== -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
-	<!-- CSS
-  ================================================== -->
-	<link rel="stylesheet" href="stylesheets/base.css">
-	<link rel="stylesheet" href="stylesheets/skeleton.css">
-	<link rel="stylesheet" href="stylesheets/layout.css">
-
-	<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-	<![endif]-->
-
-	<!-- Favicons
-	================================================== -->
-	<link rel="shortcut icon" href="images/favicon.ico">
-	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
-
-</head>
-<body>
-
-	<!-- Primary Page Layout
-	================================================== -->
-
-	<!-- Delete everything in this .container and get started on your own site! -->
-
-	<div class="container">
-		<div class="sixteen columns">
-			<h2 class="remove-bottom" style="margin-top: 40px">Leitu&Haome</h2>
-			<h5>Version 0.2.0</h5>
-			<hr />
-		</div>
-		<div class="one-third column">
-      <form action="" method="post">
-        <div class="container column">
-          <!--<textarea id="regularTextarea" name="amount"></textarea>-->
-          <input type="text" name="account" size="20" maxlength="40">
-        </div>
-        <div class="container column">
-          <input type="submit" value="upload">
-        </div>
-      </form>
-      <!--phpupload-->
-	 	<?php
+<div class="row">
+	<div class="col-md-4 col-sm-12">
+		<form class="form-horizontal" role="form" action="" method="post">
+			<div class="form-group">
+				<input type="text" name="account" size="20" maxlength="40">
+			</div>
+			<div class="form-group">
+				<input type="submit" class="btn btn-success" value="Upload">
+			</div>
+		</form>
+		<?php	 	
+		//输入message页面
 	 	require('mysqli_connect.php');
 	 		if(!empty($_POST['account'])){
 	 			$account = mysqli_real_escape_string($dbc, $_POST['account']);
@@ -72,11 +29,11 @@
 	 		}else{
 	 			$message = NULL;
                }
-//mysqli_close($dbc);
-	 	?>
-		</div>
-		<div class="two-thirds column">
-			<?php
+	     
+	     //输入完成
+		 echo '</div>';
+		 echo '<div class="col-md-8 col-sm-12">';
+
 			//require('mysqli_connect.php');
 			$q2 = "SELECT  account AS ac , DATE_FORMAT(account_date, '%m-%d-%Y ')AS dt FROM accounts ORDER BY account_date DESC;
 ";
@@ -96,9 +53,9 @@
                } 
 			mysqli_close($dbc); 
 		?>
-		</div>
-	</div><!-- container -->
-<!-- End Document
-================================================== -->
-</body>
-</html>
+
+	</div>
+</div>
+<?php
+  include ('includes/footer.html');
+?>
--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php 
 
-$page_title = 'Haome&Leitu';
-include ('includes/header.html');
-
+$page_title = 'Haome&Leitu'; //页面标题
+include ('includes/header.html'); 
 ?>
 
 <div class="row">
@@ -16,7 +15,8 @@ include ('includes/header.html');
 			</div>
 		</form>
 		<?php	 	
-		require('mysqli_connect.php');
+		//输入message页面
+		require('mysqli_connect.php'); 
 		if(!empty($_POST['message'])){
 		 	$message = mysqli_real_escape_string($dbc, $_POST['message']);
 		 	$q = "INSERT INTO haome (message_body, message_datetime) VALUES ('$message',NOW())";
@@ -29,27 +29,71 @@ include ('includes/header.html');
 		 }else{
 		 	$message = NULL;
 	        }
+	     //输入message页面完成
 		 echo '</div>';
 		 echo '<div class="col-md-8 col-sm-12">';
 
-		//require('mysqli_connect.php');
-			$q2 = "SELECT  message_body AS ms , DATE_FORMAT(message_datetime, '%H:%i:%S %m-%d-%Y ')AS dt FROM haome ORDER BY message_datetime DESC;
-		";
-			$r2 = @mysqli_query ($dbc, $q2);
-			$num = mysqli_num_rows($r2);
-			if ( $num > 0 ) {
-				echo '<div>';
-		        while ($row2 = mysqli_fetch_array($r2, MYSQLI_ASSOC)) { 
-			        echo '<h4>' . $row2 ['ms'] .'</h4>';
-			        echo '<p>' . $row2 ['dt'] . '</p>';
-			        echo '<hr />';
-		        } 
-		        echo '</div>';
+		 //分页显示
 
-		    } else {
-		        echo "<p> none </p>";
-		    } 
-			mysqli_close($dbc); 
+		 $display = 25; //每页显示25条
+		 if (isset($_GET['p']) && is_numeric($_GET['p'])) {
+		 	$pages = $_GET['p'];
+		 } else {
+		 	$q2 = "SELECT COUNT(message_id) FROM haome";
+		 	
+			$r2 = @mysqli_query ($dbc, $q2);
+			//$num = mysqli_num_rows($r2);
+			$row = @mysqli_fetch_array ($r2, MYSQLI_NUM);
+			$records = $row[0];
+
+			if ($records > $display) {
+				$pages = ceil ($records/$display);
+			} else {
+				$pages = 1;
+			}
+		 }
+
+		 if (isset($_GET['s']) && is_numeric($_GET['s'])) {
+		 	$start = $_GET['s'];
+		 } else {
+		 	$start = 0;
+		 }
+
+		 $q2 = "SELECT  message_body AS ms , DATE_FORMAT(message_datetime, '%H:%i:%S %m-%d-%Y ')AS dt FROM haome ORDER BY message_datetime DESC LIMIT $start, $display";
+		 $r2 = @mysqli_query ($dbc, $q2);
+
+		 echo '<div>';
+		 while ($row = mysqli_fetch_array($r2, MYSQLI_ASSOC)) {
+			echo '<h4>' . $row ['ms'] .'</h4>';
+			echo '<p>' . $row ['dt'] . '</p>';
+			echo '<hr />';
+		 } 
+		 echo '</div>';
+
+		 mysqli_free_result ($r2);
+ 		 mysqli_close($dbc);
+
+ 		 if ($pages >1) {
+ 				echo '<br /><p>';
+ 				$current_page = ($start/$display) + 1;
+
+ 				if ($current_page != 1) {
+ 					echo '<a href="index.php?s='. ($start - $display). '&p=' . $pages .'">Previous</a>
+ 					';
+ 				}
+
+ 				for ($i = 1; $i <= $pages; $i++) {
+ 					if ($i != $current_page){
+ 						echo '<a href="index.php?s=' .(($display*($i -1))).'&p=' .$pages. '">' . $i .' </a>';
+ 					} else {
+ 						echo $i .' ';
+ 					}
+ 				}
+
+ 				if ($current_page != $pages) {
+ 					echo '<a href="index.php?s='.($start + $display). '&p=' .$pages . '">Next</a>';
+ 				}
+ 			}
 		?>
 	</div>
 </div>
