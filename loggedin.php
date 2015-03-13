e7a6c3a45f6688ecf03060b8032cdee04e6213c5
shoto <shotolee@gmail.com>
2015-03-13 18:05:12 +0800

    add cookie

---
+++ loggedin.php
@@ -0,0 +1,18 @@
+<?php
+
+if (!isset($_COOKIE['user_id'])) {
+	require ('includes/login_function.inc.php');
+	redirect_user();
+
+}
+
+$page_title = 'Logged In!';
+include ('includes/header.html');
+
+echo "<h1>Logged In!</h1>
+<p>You are now logged in, {$_COOKIE['first_name']}!<p>
+<p><a href=\"logout.php\">Logout</a></p>";
+
+include ('includes/footer.html');
+
+?>
---
+++ logout.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!isset($_COOKIE['user_id'])) {
+
+	require ('includes/login_functions.inc.php');
+	redirect_user();
+} else {
+	setcookie ('user_id', '', time()-3600, '/', '', 0, 0);
+	setcookie ('first_name', '',time()-3600, '/', '', 0, 0); 
+}
+
+$page_title = 'Logged Out!';
+include ('includes/header.html');
+
+echo "<h1>Logged Out!</h1>
+<p>You are now logged out, {$_COOKIE['first_name']}!</p>";
+
+include ('include/footer.html');
+
+?>
